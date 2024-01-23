565a2d994f72796984047cb3e9080bb0531fe60a
ayşe <ayse.efsin@yandex.com>
2024-01-23 04:29:50 +0300

    first part done

--- src/GroceryList.java
+++ src/GroceryList.java
@@ -1,16 +1,43 @@
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
 
-public class GroceryList implements List{
+public class GroceryList{
 
-   private ArrayList groceryList  = new ArrayList();
+   private List<String> groceryList  = new ArrayList<>();
 
-   public GroceryList(ArrayList groceryList) {
-      this.groceryList = groceryList;
+   public GroceryList() {
+
+   }
+   public boolean checkItemIsInList( String item){
+      return groceryList.contains(item);
+   }
+   public void addItems(String item){
+      if(!checkItemIsInList(item)){
+         groceryList.add(item);
+      }else{
+         System.out.println("Product "+ item + " is added before! Please try to add another product.");
+      }
+
+   }
+   public void removeItems(String item){
+      if(checkItemIsInList(item)){
+         groceryList.remove(item);
+      }else{
+         System.out.println("Your shopping cart does not include this product: "+item);
+      }
+
+   }
+   public List printSorted(){
+      Collections.sort(groceryList);
+      System.out.println(groceryList);
+       return groceryList;
    }
 
-   public ArrayList addItems(String item){
-      groceryList.add(item);
-      return groceryList.sort()
+   @Override
+   public String toString() {
+      return "GroceryList{" +
+              "groceryList=" + groceryList +
+              '}';
    }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        GroceryList aysinList= new GroceryList();
+
+        aysinList.addItems("apple");
+        aysinList.addItems("banana");
+        aysinList.addItems("strawberry");
+        System.out.println("The List: " +aysinList);
+        aysinList.addItems("cheese");
+        aysinList.addItems("cheese");
+        System.out.println("The List: " +aysinList);
+        aysinList.removeItems("apple");
+        System.out.println("The List: " +aysinList);
+        aysinList.removeItems("apple");
+        System.out.println("The List: " +aysinList);
+
+
+
+
     }
 }
