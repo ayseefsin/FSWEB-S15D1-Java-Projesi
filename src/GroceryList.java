d0013ea48dd3ed77069fc8148d52bd3ed9ee94ab
ayşe <ayse.efsin@yandex.com>
2024-01-24 15:30:25 +0300

    done

--- src/GroceryList.java
+++ src/GroceryList.java
@@ -7,24 +7,42 @@ public class GroceryList{
    private List<String> groceryList  = new ArrayList<>();
 
    public GroceryList() {
-
+      this.groceryList=new ArrayList<>();
    }
+
+   private static String[] splitItems(String items) {
+      String[] splitItems = items.split(",");
+      return splitItems;
+   }
+
+
    public boolean checkItemIsInList( String item){
       return groceryList.contains(item);
    }
-   public void addItems(String item){
-      if(!checkItemIsInList(item)){
-         groceryList.add(item);
-      }else{
-         System.out.println("Product "+ item + " is added before! Please try to add another product.");
+
+
+   public void addItems(String items){
+      String[] splitItems = splitItems(items);
+      for(String item: splitItems){
+         String trimmedItem = item.trim();
+         if(!checkItemIsInList(trimmedItem)){
+            groceryList.add(trimmedItem);
+            sortItems();
+         }else{
+
+            System.out.println("Product "+ items + " is added before! Please try to add another product.");
+         }
       }
 
+
    }
-   public void removeItems(String item){
-      if(checkItemIsInList(item)){
-         groceryList.remove(item);
+
+
+   public void removeItems(String items){
+      if(checkItemIsInList(items)){
+         groceryList.remove(items);
       }else{
-         System.out.println("Your shopping cart does not include this product: "+item);
+         System.out.println("Your shopping cart does not include this product: "+items);
       }
 
    }
@@ -33,7 +51,9 @@ public class GroceryList{
       System.out.println(groceryList);
        return groceryList;
    }
-
+   public void sortItems() {
+      Collections.sort(groceryList);
+   }
    @Override
    public String toString() {
       return "GroceryList{" +
--- src/Main.java
+++ src/Main.java
@@ -19,12 +19,46 @@ public class Main {
         System.out.println("The List: " +aysinList);
         aysinList.removeItems("apple");
         System.out.println("The List: " +aysinList);
-        Scanner scanner = new Scanner(System.in);
-        String productName;
-        System.out.println("Please write the product you want to add to your shopping list in the box below.");
-        productName= scanner.nextLine();
-        aysinList.addItems(productName);
-        System.out.println("Your shopping card : " + aysinList);
+        Scanner scannerInitial = new Scanner(System.in); //Create scanner object
+        int message1;// describe a string and named
+        do {
+            System.out.println("Please press 0 to stop, 1 to add, 2 to remove");//message to user
+            message1= Integer.parseInt(scannerInitial.nextLine());
+
+            switch (message1){
+                case 0:
+                    System.out.println("You stopped the application. Your final shopping card list: " + aysinList);
+                    ;
+                    break;
+                case 1:
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.println("Please write the product you want to add to your shopping " +
+                            "list in the box below then press enter");
+                    String productName;
+                    productName= scanner.nextLine();
+                    aysinList.addItems(productName);
+                    System.out.println("Your shopping card : " + aysinList);
+                    break;
+                case 2:
+                    Scanner scannerRemove = new Scanner(System.in);
+                    System.out.println("Please write the product you want to remove to your shopping " +
+                            "list in the box below, then press enter");
+                    String productNameRemoved;
+                    productNameRemoved= scannerRemove.nextLine();
+                    aysinList.removeItems(productNameRemoved);
+                    System.out.println("Your shopping card : " + aysinList);
+                    break;
+                default: System.out.println("Invalid entry. Please press 0, 1 or 2 then press enter");
+            }
+        }while (message1!=0);
+
+
+
+
+
+
+
+
 
 
 
